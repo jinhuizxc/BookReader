94d8a4fc841ed5edbdbbdba44ab1244647b30724
Administrator <1004260403@qq.com>
2017-04-21 14:15:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/bookreader/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.bookreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 本周：即4.23-4.27号要开始写这个项目：任阅
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
